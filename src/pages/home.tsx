860e02553702ad6b1b41a3c168e2fbb5e30612f9
JoseRivero <josearmandorr95@gmail.com>
2022-06-01 21:44:13 +0200

    Initial Commit

---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+export function HomePage() {
+  const template = (
+    <div>
+      <img
+        height="400px"
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        alt=""
+      />
+    </div>
+  );
+  return template;
+}
